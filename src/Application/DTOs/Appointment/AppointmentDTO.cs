f8c7d813d713adcb8fee667719a481933ea8a2e9
Maira Scomparim <maira@Mairas-MacBook-Pro.local>
2025-03-27 21:15:03 -0300

    Adicionando Dtos Appointment e Dtos Auxiliares

---
+++ src/Application/DTOs/Appointment/AppointmentDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinAgenda.src.Application.DTOs.Appointment
+{
+    public class AppointmentDTO
+    {
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public int SpecialtyId { get; set; }
+        public required string AppointmentDate { get; set; }
+        public required string Observation { get; set; }
+    }
+}
